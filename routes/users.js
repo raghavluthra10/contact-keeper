8a78b0b19e4284944cb11520d1770110b83b83c0
raghav786 <58934695+raghav786@users.noreply.github.com>
2021-07-22 15:44:46 +0530

    user registration complete

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const config = require("config");
+const jwt = require('jsonwebtoken');
+
 
 // bringing in UserSchema
 const User = require('../models/User');
@@ -45,7 +48,19 @@ router.post('/', [
         // save user to database
         await user.save();
 
-        res.send('User saved')
+        // Payload will contain the object that will be sent into the jsonwebtoken
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(payload, config.get('jwtSecret'), {
+            expiresIn: 360000
+        }, (err, token) => {
+            if(err) throw err;
+            res.json({token});
+        })
 
     } catch (err) {
         console.log(err.message);
