85f1e9335600490036cf9f4e0d687ed24ac0ed7d
raghav786 <58934695+raghav786@users.noreply.github.com>
2021-07-26 17:39:33 +0530

    state, add, delete, edit, contact form

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import ContactContext from '../../context/contact/contactContext';
 
 const ContactItem = ({ contact }) => {
 
+    const contactContext = useContext(ContactContext)
+    const { deleteContact, setCurrent, clearCurrent } = contactContext;
+
     const { name, id, email, phone, type } = contact
+
+    const onDelete  = () => {
+        deleteContact(id);
+        clearCurrent();
+    };
 
     return (
         <div className='card bg-light'>
             <h3 className='text-primary text-left'> 
-                {name.charAt(0).toUpperCase() + name.slice(1)}{' '} 
+                {name && name.charAt(0).toUpperCase() + name.slice(1)} {' '} 
                     <span 
                         style={{ float:'right' }}
                         className={'badge' + (type === 'professional' ? 'badge-success' : 'badge-primary')}>
-                        {type.charAt(0).toUpperCase() + type.slice(1)} 
+                        {type && type.charAt(0).toUpperCase() + type.slice(1)} 
                     </span>
             </h3>
 
@@ -30,8 +39,8 @@ const ContactItem = ({ contact }) => {
                 )}
             </ul>
             <p>
-                <button className='btn btn-dark btn-sm'> Edit </button>
-                <button className='btn btn-danger btn-sm'> Delete </button>
+                <button className='btn btn-dark btn-sm' onClick={() => setCurrent(contact)}> Edit </button>
+                <button className='btn btn-danger btn-sm' onClick={onDelete} > Delete </button>
             </p>
         </div>
     )
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react'
-import uuid from 'uuid';
+import {v4 as uuid} from "uuid"; 
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 import { ADD_CONTACT, DELETE_CONTACT, SET_CURRENT, CLEAR_CURRENT, UPDATE_CONTACT, FILTER_CONTACTS, CLEAR_FILTER } from '../types';
@@ -29,20 +29,37 @@ const ContactState = (props) => {
                 email: 'bobby@bobby.com',
                 type: 'professional'
             }
-        ]
+        ],
+        current: null
     }
 
     const [ state, dispatch ] = useReducer( contactReducer, initialState );
 
     // Add Contact
+    const addContact = contact => {
+        contact.id = uuid();
+        dispatch({ type: ADD_CONTACT, payload: contact })
+    };
 
     // Delete Contact
+    const deleteContact = id => {
+        dispatch({ type: DELETE_CONTACT, payload: id })
+    };
 
     // set CUrrent Contact
+    const setCurrent = contact => {
+        dispatch({ type: SET_CURRENT, payload: contact })
+    };
 
     // clear contacts
+    const clearCurrent = contact => {
+        dispatch({ type: CLEAR_CURRENT })
+    };
 
     // update contact
+    const updateContact = contact => {
+        dispatch({ type: UPDATE_CONTACT, payload: contact })
+    };
 
     // filter contacts
 
@@ -50,7 +67,13 @@ const ContactState = (props) => {
 
     return (
         <ContactContext.Provider value={{
-            contacts: state.contacts
+            contacts: state.contacts,
+            current: state.current,
+            addContact,
+            deleteContact,
+            clearCurrent,
+            setCurrent,
+            updateContact
         }}>
             {props.children}
         </ContactContext.Provider>
